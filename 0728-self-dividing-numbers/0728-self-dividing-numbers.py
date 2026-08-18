7b3112bda2f6fb031b3cbc0c0d6521aa770d2522
Pittala Shashanth <shashanthnetha@gmail.com>
2026-08-18 20:56:43 +0530

    Time: 7 ms (54.84%), Space: 19.3 MB (74.22%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -1,18 +1,23 @@
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        arr=[]
-        for i in range(left,right+1):
-            n=len(str(i))
-            count=0
+        arr = []
+
+        for i in range(left, right + 1):
+
+            valid = True
+
             for j in str(i):
-                if int(j)==0:
-                    continue
-                if i%int(j)==0:
-                    count+=1
-                else:
-                    count=0
-            if count==n:
+
+                digit = int(j)
+
+                if digit == 0 or i % digit != 0:
+                    valid = False
+                    break
+
+            if valid:
                 arr.append(i)
+
+        return arr
         return arr
 
         
